fb16096fda0626bfac1cd382fb6667089afc30aa
Sagar <sag28.01.2001@gmail.com>
2025-05-31 00:35:58 +0530

    made post card into reusable component

---
+++ components/PostCard.vue
@@ -0,0 +1,52 @@
+<template>
+    <div
+        class="bg-gray-100 flex items-center justify-center p-4 cursor-pointer"
+    >
+        <div class="bg-white max-w-xl w-full rounded-xl shadow p-6 space-y-4">
+            <h2 class="text-2xl font-bold text-gray-900">
+                {{ x.title }}
+            </h2>
+
+            <div
+                class="flex justify-between items-center text-sm text-gray-500"
+            >
+                <div>👤 {{ x.userId }}</div>
+                <div>{{ x.views }} views</div>
+            </div>
+
+            <div class="flex flex-wrap gap-2">
+                <span
+                    v-for="tag in x.tags"
+                    class="bg-gray-100 text-gray-800 px-3 py-1 rounded-full text-sm"
+                    >{{ tag }}</span
+                >
+            </div>
+
+            <div
+                class="flex justify-between items-center pt-2 text-gray-600 text-sm"
+            >
+                <div class="flex gap-6 items-center">
+                    <div class="flex items-center gap-1">
+                        <span>👍</span>
+                        <span>{{ x.reactions.likes }}</span>
+                    </div>
+                    <div class="flex items-center gap-1">
+                        <span>👎</span>
+                        <span>{{ x.reactions.dislikes }}</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script setup>
+    defineProps({
+        x: {
+            type: Object,
+            required: true,
+        },
+    });
+</script>
+
+<style lang="scss" scoped></style>
--- pages/posts/index.vue
+++ pages/posts/index.vue
@@ -1,57 +1,20 @@
 <script setup>
+    import PostCard from '~/components/PostCard.vue';
     import { useDataStore } from '~/stores/dataStore';
     const store = useDataStore();
-    const { BigData, navigateToPost } = store;
-    console.log(BigData.posts);
+    const { navigateToPost, BigData } = store;
 </script>
 <template>
     <div class="text-center">
         <p class="text-[#89CFF0] text-3xl font-bold p-4">This is posts page</p>
 
         <div class="grid grid-cols-3">
-            <div
-                v-for="x in BigData.posts"
-                @click="navigateToPost(x.id)"
-                class="bg-gray-100 flex items-center justify-center p-4 cursor-pointer"
-            >
-                <div
-                    class="bg-white max-w-xl w-full rounded-xl shadow p-6 space-y-4"
-                >
-                    <h2 class="text-2xl font-bold text-gray-900">
-                        {{ x.title }}
-                    </h2>
-
-                    <div
-                        class="flex justify-between items-center text-sm text-gray-500"
-                    >
-                        <div>👤 {{ x.userId }}</div>
-                        <div>{{ x.views }} views</div>
-                    </div>
-
-                    <div class="flex flex-wrap gap-2">
-                        <span
-                            v-for="tag in x.tags"
-                            class="bg-gray-100 text-gray-800 px-3 py-1 rounded-full text-sm"
-                            >{{ tag }}</span
-                        >
-                    </div>
-
-                    <div
-                        class="flex justify-between items-center pt-2 text-gray-600 text-sm"
-                    >
-                        <div class="flex gap-6 items-center">
-                            <div class="flex items-center gap-1">
-                                <span>👍</span>
-                                <span>{{ x.reactions.likes }}</span>
-                            </div>
-                            <div class="flex items-center gap-1">
-                                <span>👎</span>
-                                <span>{{ x.reactions.dislikes }}</span>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
+            <PostCard
+                v-for="post in BigData.posts"
+                :key="post.id"
+                :x="post"
+                @click="navigateToPost(post.id)"
+            />
         </div>
     </div>
 </template>
