62d04786b9c3e05fe770e2ccb070bf1f61151a4c
Sagar <sag28.01.2001@gmail.com>
2025-05-30 21:43:03 +0530

    update API endpoint and improve posts display

--- pages/posts.vue
+++ pages/posts.vue
@@ -1,14 +1,17 @@
 <script setup>
     import { useDataStore } from '~/stores/dataStore';
-    const dataStore = useDataStore();
-    let { BigData } = dataStore;
-    // console.log(dataStore.BigData);
+    const store = useDataStore();
+    const { BigData } = store;
+    console.log(BigData.posts);
 </script>
 <template>
-    This is posts page
-    <div class="grid grid-cols-5">
-        <div v-for="x in BigData" class="mb-5">
-            {{ x.body }}
+    <div class="text-center">
+        This is posts page
+        <div class="grid grid-cols-5">
+            <div v-for="x in BigData.posts" class="mb-5 border-2 text-center">
+                {{ x.id }}
+                {{ x.title }}
+            </div>
         </div>
     </div>
 </template>
--- stores/dataStore.js
+++ stores/dataStore.js
@@ -1,8 +1,9 @@
 import {defineStore} from 'pinia';
 export const useDataStore= defineStore('data',()=>{
     let BigData = ref([]);
-    const {data,pending,error} = useFetch('https://jsonplaceholder.typicode.com/posts')
+    const {data,pending,error} = useFetch('https://dummyjson.com/posts')
     BigData.value = data;
+    console.log(BigData.value); 
     return {
         BigData
     }
